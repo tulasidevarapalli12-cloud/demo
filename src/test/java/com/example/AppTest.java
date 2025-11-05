21d7ee1ad17777fc1a2b2b1417c1b058713b911e
tulasi.devarapalli <tulasi.devarapalli@accenture.com>
2025-11-06 01:21:37 +0530

    og code

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
+
